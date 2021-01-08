e326ff3a1d4c5ebb38bf4e4c6241e443e6eb4e21
Allen70 <Allenlea2007@gmail.com>
2021-01-08 10:16:45 -0700

    added dungeon intro and started creating a main menu

--- gameone.js
+++ gameone.js
@@ -46,8 +46,11 @@ function seekUserInput(currentBalance, cost) {
 function enterDungeon() {
     let arcade = document.querySelector('.arcade')
     arcade.classList.add('hidden')
+    let link = document.createElement('link')
+    link.href = "dungeon.css"
+    link.rel = 'stylesheet'
     let script = document.createElement('script')
     script.src = "dungeon.js"
-    document.body.append(script)
+    document.body.append(link,script)
 
 }
